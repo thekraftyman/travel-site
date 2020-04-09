8dff5887243f1f5a969f3770d74f6177fc124527
Adam <thekraftyman@gmail.com>
2020-04-09 14:18:19 -0500

    created some fade-in js and applied it to both features and testimonials

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(".feature-item", 85);
+new RevealOnScroll(".testimonial", 70);
 
 if (module.hot) {
   module.hot.accept();
