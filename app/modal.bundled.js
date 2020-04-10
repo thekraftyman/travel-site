045cd957f04cd570a0d38e77064ef42f0062a36f
Adam <thekraftyman@gmail.com>
2020-04-10 09:54:16 -0500

    completed build process config

---
+++ app/modal.bundled.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["modal"],{
+
+/***/ "./app/assets/scripts/modules/Modal.js":
+/*!*********************************************!*\
+  !*** ./app/assets/scripts/modules/Modal.js ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nclass Modal {\r\n  constructor () {\r\n    // html injector\r\n    this.injectHTML();\r\n\r\n    // buttons and constants\r\n    this.modal = document.querySelector(\".modal\");\r\n    this.closeIcon = document.querySelector(\".modal__close\")\r\n\r\n    // events to call\r\n    this.events();\r\n  }\r\n\r\n  events() {\r\n    // open modal button click\r\n    // close modal button click\r\n    this.closeIcon.addEventListener(\"click\",() => this.closeTheModal());\r\n    // any button click\r\n    document.addEventListener(\"keyup\", e => this.checkKey(e));\r\n  }\r\n\r\n  checkKey(e) {\r\n    // checks the keypress and determines if it escape or not and closes if it is\r\n    if (e.keyCode == 27) {\r\n      this.closeTheModal();\r\n    }\r\n  }\r\n\r\n  openTheModal () {\r\n    // open the modal function\r\n    // make the modal visible\r\n    this.modal.classList.add(\"modal--is-visible\");\r\n  }\r\n\r\n  closeTheModal () {\r\n    // function to close the modal by selecting the x\r\n    this.modal.classList.remove(\"modal--is-visible\");\r\n  }\r\n\r\n  injectHTML () {\r\n    document.body.insertAdjacentHTML('beforeend',`\r\n      <div class=\"modal\">\r\n        <div class=\"modal__inner\">\r\n          <h2 class=\"section-title section-title--blue section-title--less-margin\"><img src=\"assets/images/icons/mail.svg\" class=\"section-title__icon\"> Get in <strong>Touch</strong></h2>\r\n          <div class=\"wrapper wrapper--narrow\">\r\n            <p class=\"modal__description\">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>\r\n          </div>\r\n\r\n          <div class=\"social-icons\">\r\n            <a href=\"#\" class=\"social-icons__icon\"><img src=\"assets/images/icons/facebook.svg\" alt=\"Facebook\"></a>\r\n            <a href=\"#\" class=\"social-icons__icon\"><img src=\"assets/images/icons/twitter.svg\" alt=\"Twitter\"></a>\r\n            <a href=\"#\" class=\"social-icons__icon\"><img src=\"assets/images/icons/instagram.svg\" alt=\"Instagram\"></a>\r\n            <a href=\"#\" class=\"social-icons__icon\"><img src=\"assets/images/icons/youtube.svg\" alt=\"YouTube\"></a>\r\n          </div>\r\n        </div>\r\n        <div class=\"modal__close\">X</div>\r\n      </div>\r\n    `)\r\n  }\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (Modal);\r\n\n\n//# sourceURL=webpack:///./app/assets/scripts/modules/Modal.js?");
+
+/***/ })
+
+}]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-const path = require('path')
+const currentTask = process.env.npm_lifecycle_event;
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
+const path = require('path');
 const postCSSplugins = [
   require('postcss-import'),
   require('postcss-mixins'),
@@ -7,13 +12,47 @@ const postCSSplugins = [
   require('postcss-nested'),
   require('autoprefixer')
 ]
-module.exports = {
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy images', function () {
+      fse.copySync('./app/assets/images','./docs/assets/images')
+    })
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader', {loader: 'postcss-loader',options: {plugins: postCSSplugins}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+  return file.endsWith('.html')
+}).map(function (page) {
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+  })
+})
+
+
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+};
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname,'app')
-  },
-  devServer: {
+  };
+  config.devServer = {
     before: function(app,server){
       server._watch('./app/**/*.html')
     },
@@ -21,14 +60,36 @@ module.exports = {
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader','css-loader', {loader: 'postcss-loader',options: {plugins: postCSSplugins}}]
+  };
+  config.mode = 'development';
+
+} else if (currentTask == 'build') {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
       }
-    ]
-  }
+    }
+  })
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSplugins.push(require('cssnano'))
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname,'docs')
+  };
+  config.mode = 'production';
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  };
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()
+  )
 }
+
+module.exports = config;
