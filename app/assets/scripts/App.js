22d5489e56540c45eb697b00466f39371a3eba76
Adam <thekraftyman@gmail.com>
2020-04-09 19:56:14 -0500

    created the modal and added controls to it

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,6 +7,21 @@ let mobileMenu = new MobileMenu();
 new RevealOnScroll(".feature-item", 85);
 new RevealOnScroll(".testimonial", 70);
 new StickyHeader();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click",e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import('./modules/Modal').then(x => {
+        modal = new x.default();
+        setTimeout(() => modal.openTheModal(), 20);
+      }).catch(() => console.log("There was a problem downloading the modal package"));
+    } else {
+      modal.openTheModal();
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept();
